7e7dc3a7db0bdd7bfac4a122309e38168ec03b57
Nick Strayer <n.strayer@vanderbilt.edu>
2018-08-16 12:09:19 -0400

    Update get_packages.sh

--- get_packages.sh
+++ get_packages.sh
@@ -1,3 +1,3 @@
 sudo apt-get update
 
-sudo apt-get install libcurl4-openssl-dev libssl-dev libxml2-dev 
+sudo apt-get install libclang-dev r-base r-base-dev libcurl4-openssl-dev libssl-dev libxml2-dev 
